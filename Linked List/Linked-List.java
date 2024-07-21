847d6c1bf1e2bcd129f7475ee228ac22ee7d2f7a
𝐇𝐞𝐛𝐚 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-21 23:56:13 +0300

    linked list

---
+++ Linked List/Linked-List.java
@@ -0,0 +1,75 @@
+
+package course ;
+import java.util.*;
+import javafx.scene.Node;
+public class LinkedList {
+    
+    private Node head;
+    private Node tail;
+    private int length;
+     
+    class Node {
+        int value;
+        Node next;
+     
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+    
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+}
+
+// main 
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(4);
+        
+        myLinkedList.printAll();
+        
+    }}
